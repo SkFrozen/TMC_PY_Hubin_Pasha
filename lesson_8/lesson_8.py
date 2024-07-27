e57881ca04681b9e8261faff945291481f1a143a
SkFrozen <frozentw91@gmail.com>
2024-07-27 10:10:28 +0300

    lesson_8 have been completed

--- lesson_8/lesson_8.py
+++ lesson_8/lesson_8.py
@@ -1,2 +1,23 @@
-print('Hello world!')
+def calculater(number_1: int, number_2: int, action: str) -> int:
+    """Receives 2 numbers and performes an action and return result"""
+    if action == "-":
+        return number_1 - number_2
+    elif action == "+":
+        return number_1 + number_2
+    elif action == "*":
+        return number_1 * number_2
+    elif action == "/":
+        return number_1 / number_2
 
+
+try:
+    num_1 = float(input("Enter first number: "))
+    num_2 = float(input("Enter second number: "))
+    action = input("Enter an action what do you like to use, example (+, -, *, /): ")
+    if "+, -, *, /".find(action) == -1:
+        raise TypeError("Action not found")
+    print(round(calculater(num_1, num_2, action), 3))
+except ZeroDivisionError as e:
+    print("Cannot divide by zero:", e)
+except ValueError as e:
+    print("Enter valid data:", e)
