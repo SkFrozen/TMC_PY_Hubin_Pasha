34d687fee4f5bf0a09138e41e03c3758ce9b6224
Hubin Pasha <frozentw91@gmail.com>
2024-08-19 16:40:07 +0300

    modified lesson_11

--- lesson_11/exercise_1.py
+++ lesson_11/exercise_1.py
@@ -1,11 +1,12 @@
 class Soda:
-    
+
     def __init__(self, taste="usual"):
         self.taste = taste
-    
+
     def what_is_taste(self):
         return f"You have a soda with the {self.taste} taste"
-    
-usual = Soda("strawbarry")
+
+
+usual = Soda("strawberry")
 
 print(usual.what_is_taste())
