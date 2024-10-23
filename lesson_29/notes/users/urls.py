aa421352ff29de74d929bfcc312ee3bf93ef9755
SkFrozen <frozentw91@gmail.com>
2024-10-23 09:59:24 +0300

    deleted lesson_29

--- lesson_29/notes/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.UserListCreateAPIView.as_view(), name="user_list_create"),
-    path("delete/<int:pk>/", views.UserDestroyAPIView.as_view(), name="user_detail"),
-]
